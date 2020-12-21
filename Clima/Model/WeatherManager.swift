390e7bcecabc155269d6938688dee7c24830f0da
njdeane <njdeane7@gmail.com>
2020-12-21 15:19:03 +1000

    create WeatherModel to hold and work out condition icons to send to UI

--- Clima/Model/WeatherManager.swift
+++ Clima/Model/WeatherManager.swift
@@ -11,6 +11,8 @@ import Foundation
 struct WeatherManager {
     let weatherURL = ""
     
+    var delegate: WeatherManagerDelegate?
+    
     func fetchWeather(cityName: String) {
         let urlString = "\(weatherURL)&q=\(cityName)"
         performRequest(urlString: urlString)
@@ -25,25 +27,35 @@ struct WeatherManager {
                     return
                 }
                 if let safeData = data {
-                    self.parseJSON(weatherData: safeData)
+                    if let weather = self.parseJSON(weatherData: safeData) {
+                        delegate?.didUpdateWeather(weather: weather)
+                    }
                 }
             }
             task.resume()
         }
     }
     
-    func parseJSON(weatherData: Data) {
+    func parseJSON(weatherData: Data) -> WeatherModel? {
         let decoder = JSONDecoder()
         do {
             let decodedData = try decoder.decode(WeatherData.self, from: weatherData)
-            print(decodedData.weather[0].description)
+            let id = decodedData.weather[0].id
+            let temp = decodedData.main.temp
+            let name = decodedData.name
+            
+            let weather = WeatherModel(conditionId: id, cityName: name, temperature: temp)
+            return weather
         } catch {
              print(error)
+            return nil
         }
     }
     
     
+    
 }
+
 
 
 
